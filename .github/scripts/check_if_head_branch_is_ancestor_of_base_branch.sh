06573a103d897578727a06e9fe753a6b8e387ca3
Ilya Gulyayev <ilya.gulyayev@simbirsoft.com>
2024-06-21 13:08:30 +0300

    add sc

--- .github/scripts/check_if_head_branch_is_ancestor_of_base_branch.sh
+++ .github/scripts/check_if_head_branch_is_ancestor_of_base_branch.sh
@@ -6,7 +6,7 @@ HEAD=$2
 
 git fetch origin $BASE
 
-if  !(git merge-base --is-ancestor  $HEAD $BASE) ; then
+if ! git merge-base --is-ancestor  $HEAD $BASE; then
     bash ./.github/scripts/send_issue.sh $COMMENTS_URL $GITHUB_TOKEN $MESSAGE
     exit 1
 fi
