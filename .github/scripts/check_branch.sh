ffba3c4b67f81a1c0c37c932c729a0f0e632766e
Ilya Gulyayev <ilya.gulyayev@simbirsoft.com>
2024-06-21 12:16:55 +0300

    add sc

--- .github/scripts/check_branch.sh
+++ .github/scripts/check_branch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if [[ $1 == 'master' && ($2 != 'develop' && !($2 =~ 'hotfix/.*')) ]]; then
+  echo $COMMENTS_URL
   curl \
     -X POST \
     $COMMENTS_URL \
     -H "Content-Type: application/json" \
     -H "Authorization: token $GITHUB_TOKEN" \
     --data '{ "body":"Пр может быть только от ветки develop или hotfix/**" }'
-  exit 1
+  # exit 1
 fi
