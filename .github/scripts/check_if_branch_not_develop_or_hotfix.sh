a4b121d438d34d1bb5002e18d55b768581ca2d99
Ilya Gulyayev <ilya.gulyayev@simbirsoft.com>
2024-06-21 12:37:20 +0300

    add sc

--- .github/scripts/check_if_branch_not_develop_or_hotfix.sh
+++ .github/scripts/check_if_branch_not_develop_or_hotfix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo $1 $2
 if [[ $1 == 'master' && ($2 != 'develop' && !($2 =~ 'hotfix/.*')) ]]; then
   bash ./.github/scripts/send_issue.sh $COMMENTS_URL $GITHUB_TOKEN $MESSAGE
   exit 1
